4e0bbf0807a6be814e151b2e9a5585c1e4cad3a8
aimee-liang <aimeeeliang@gmail.com>
2020-08-29 15:33:04 -0400

    First commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        # if this exists
+        if @roster[grade]
+        else 
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+
+    def grade(year)
+        # access info in array
+        @roster[year]
+    end
+
+    def sort
+        # iterate and sort array in hash
+        sorted_students_in_hash = {}
+        @roster.each do |grade, name|
+            sorted_students_in_hash[grade] = name.sort
+        end
+        sorted_students_in_hash
+    end
+
+end
